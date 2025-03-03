54d1d5eb5286aed74f0c82cb7c6c175f6f52888e
Sweta Verma <shhmvav@gmail.com>
2025-03-02 17:04:45 -0800

    Adding reverse s string code

---
+++ src/logicalQuestions/src/logicalQuestions/reverseAString.java
@@ -0,0 +1,22 @@
+package logicalQuestions;
+
+public class reverseAString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="Sweta";
+		/*int length=0;
+		char rev;
+		
+		for(int i=str.length()-1; i>=0;i--) {
+			
+			rev= str.charAt(i);
+			
+			System.out.println(rev);*/
+		
+		
+		
+		}
+
+	}
