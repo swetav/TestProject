0fe984303bf67c414cdd45179792a05ee1c99d85
Sweta Verma <shhmvav@gmail.com>
2025-03-03 23:30:58 -0800

    Fibonacci Series

---
+++ src/logicalQuestions/src/logicalQuestions/FibonacciSeries.java
@@ -0,0 +1,22 @@
+package logicalQuestions;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=0;
+		int b=1;
+		int c;
+		
+		for(int i=1;i<=10;i++) {
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+			
+		}
+
+	}
+
+}
