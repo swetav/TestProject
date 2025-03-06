3fcf0aa2a33e819f98931547f31a9083d435ddbd
Sweta Verma <shhmvav@gmail.com>
2025-03-06 12:15:00 -0800

    Method test

---
+++ src/logicalQuestions/src/logicalQuestions/MethodDemo.java
@@ -0,0 +1,20 @@
+package logicalQuestions;
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MethodDemo one = new MethodDemo();
+		
+		one.test();
+		one.test2();
+	}
+
+public void test() {
+	System.out.println("I am a test method");
+}
+public void test2() {
+	System.out.println("I am a test two method");
+}
+}
