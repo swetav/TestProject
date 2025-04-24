fb8b034d2c7860c770f0f7f283ffee7e16d041ed
Sweta Verma <shhmvav@gmail.com>
2025-04-24 05:26:47 -0700

    MultiplicationTable

---
+++ src/logicalQuestions/src/logicalQuestions/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package logicalQuestions;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int no=2;
+for(int i=1;i<=10;i++) {
+	System.out.println(no + "X" + i + "=" + i*no);
+}
+		
+	}
+
+}
