9448b6d8fea44a9c371d4b1dcfde7a9ca9e5f896
Sweta Verma <shhmvav@gmail.com>
2025-04-24 04:55:37 -0700

    Largest of three numbers.

---
+++ src/logicalQuestions/src/logicalQuestions/LargestOfThreeNumbersInJava.java
@@ -0,0 +1,24 @@
+package logicalQuestions;
+
+public class LargestOfThreeNumbersInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
+		if(a>b && a>c) {
+			System.out.println("a is largest");
+		}
+		else if(b>a && b>c) {
+			System.out.println("b is largest");
+		}
+		else {
+			System.out.println("c is largest");
+		}
+		
+
+	}
+
+}
