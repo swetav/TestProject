8a60a562b5a52b6b78012c7a2a2cede78e5598ad
Sweta Verma <shhmvav@gmail.com>
2025-04-25 05:33:34 -0700

    Star Pattern four

--- src/logicalQuestions/src/logicalQuestions/StarPattern4.java
+++ src/logicalQuestions/src/logicalQuestions/StarPattern4.java
@@ -6,11 +6,11 @@ public class StarPattern4 {
 		// TODO Auto-generated method stub
 
 		for (int i = 0; i <= 5; i++) {
-			for (int j=4; j>= i; j--) 
+			for (int j=4; j>= i; j--)
 			{
 				System.out.print("-");
 			}
-			for (int k = 1; k <= i; k++) 
+			for (int k = 1; k <= i; k++)
 			{
 				System.out.print("*");
 			}
