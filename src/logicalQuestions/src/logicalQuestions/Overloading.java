41e87fa4983711a9a3578a98d63ef7b0bacd7ab4
Sweta Verma <shhmvav@gmail.com>
2025-06-17 19:15:44 -0700

    Method Overloading

---
+++ src/logicalQuestions/src/logicalQuestions/Overloading.java
@@ -0,0 +1,20 @@
+package logicalQuestions;
+
+public class Overloading {
+	
+	void add(int a, int b) {
+		System.out.println(a+b);	
+		}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Overloading obj = new Overloading(); {
+			obj.add(10, 20);
+		}
+
+	}
+
+}
