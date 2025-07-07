169a331817e228990576d752adebfbc72765a3d3
Sweta Verma <shhmvav@gmail.com>
2025-07-07 01:09:44 -0700

    Binary search

---
+++ src/logicalQuestions/src/logicalQuestions/BinarySearch.java
@@ -0,0 +1,27 @@
+package logicalQuestions;
+
+public class BinarySearch { //it works only on sorted arrays
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr= {10,20,30,40,50,60,70,80,90};
+		
+		int low=0;
+		int high=0;
+		
+		int element=60;
+		boolean status=false;
+		
+		for(int i=0; i<arr.length;i++) {
+			int mid=(low=high)/2;
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
